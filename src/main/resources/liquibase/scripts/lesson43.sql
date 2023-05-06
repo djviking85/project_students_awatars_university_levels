bf8916107a53720e0447f259b867d7a2b5546d3a
Viking <djviking@yandex.ru>
2023-05-06 12:29:05 +0300

    4.3 готово индексы

--- src/main/resources/liquibase/scripts/lesson43.sql
+++ src/main/resources/liquibase/scripts/lesson43.sql
@@ -1,14 +1,11 @@
-- liquibase formatted sql
-- changeset student:1
+-- - liquibase formatted sql
+-- - changeset student:1
 
-CREATE index student_name_index on Student (name);
+CREATE index student_name_index on student (name);
 
 
--changeset chernenkov:2
+-- -changeset chernenkov:2
 CREATE INDEX faculty_color_index ON faculty(color, name);
 
 
-- changeset chernenkov:3
---     удаляем индексы
-DROP INDEX faculty_color_index;
-drop index student_name_index;
+
