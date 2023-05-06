7cb192db1aac8ee60634c1955608b24d469f41dc
Viking <djviking@yandex.ru>
2023-05-06 23:14:04 +0300

    4.4 логирование

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info")
 
 public class InfoController {
+    private final Logger logger = LoggerFactory.getLogger(InfoController.class);
     @Value("${server.port}")
     private Integer port;
     @GetMapping("/port")
     public Integer getPort() {
+        logger.info("we are started prtogramm on port : = {}", port);
         return port;
     }
 }
