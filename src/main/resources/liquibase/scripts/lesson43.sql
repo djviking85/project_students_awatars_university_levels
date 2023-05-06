8d5a77aaf223947acb05727437ae2586c6e0e9ac
Viking <djviking@yandex.ru>
2023-05-06 11:56:59 +0300

    4.3 готово индексы

---
+++ src/main/resources/liquibase/scripts/lesson43.sql
@@ -0,0 +1,13 @@
+- changeset student:1
+
+CREATE index student_name_index on Student (name);
+
+
+-changeset chernenkov:2
+CREATE INDEX faculty_color_index ON faculty(color, name);
+
+
+- changeset chernenkov:3
+--     удаляем индексы
+DROP INDEX faculty_color_index;
+drop index student_name_index;
